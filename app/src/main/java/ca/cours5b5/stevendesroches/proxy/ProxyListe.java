d9c15bb8a10773e0997eafa9cbcc0c76941d6d17
Steven <stevendesroches@hotmail.com>
2018-11-20 01:10:16 -0500

    FAIT: * Ajout du bouton pour jouer en ligne * Ajout des différentes méthodes pour pouvoir aller vers l'intention de APartieReseau dans AMenuPrincipal. * Ajout ou modification des méthodes des classes de listeproxy, proxy, ControleurPartieReseau, MPartie, MPartieReseau pour pouvoir répondre aux éxigences du tp et pour pouvoir fonctionner correctement.
    * Présentement, l'application communique correctement avec le serveur firebase (avec les deux appareils), mais l'application à des bugs pour recevoir les informations.
    
    FIX:
    * Correction des constantes pour le fonctionnement de l'atelier 13
    
    BUG:
    * noeudServeur.push() n'envoit pas une clé pour moi. je dois faire noeudServeur.push().setvalue(valeur), je me servais de la clé pour pouvoir recevoir mes coups. Ducoup il y un bug pour "recevoir_coup"
    
    TODO:
    * faire une pause pour attendre la réponse de l'adversaire.
    * Corriger "recevoir_coup"

--- app/src/main/java/ca/cours5b5/stevendesroches/proxy/ProxyListe.java
+++ app/src/main/java/ca/cours5b5/stevendesroches/proxy/ProxyListe.java
@@ -1,16 +1,21 @@
 package ca.cours5b5.stevendesroches.proxy;
 
+import android.util.Log;
+
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ca.cours5b5.stevendesroches.controleurs.Action;
+import ca.cours5b5.stevendesroches.controleurs.ControleurAction;
 import ca.cours5b5.stevendesroches.controleurs.interfaces.Fournisseur;
 import ca.cours5b5.stevendesroches.global.GCommande;
+import ca.cours5b5.stevendesroches.global.GConstantes;
 
 public class ProxyListe extends Proxy implements Fournisseur {
 
@@ -18,20 +23,25 @@ public class ProxyListe extends Proxy implements Fournisseur {
 
     private Query requete;
 
-    private Action actionNouvelItem;
+    private Action actionNouvelItem = ControleurAction.demanderAction(GCommande.RECEVOIR_COUP_RESEAU);
 
-    private List<DatabaseReference> noeudsAjoutes;
+    private String key = null;
+
+    private List<DatabaseReference> noeudsAjoutes = new ArrayList<>();
 
     public ProxyListe(String cheminServeur){
         super(cheminServeur);
     }
 
     public void setActionNouvelItem(GCommande commande){
+        actionNouvelItem = ControleurAction.demanderAction(commande);
 
     }
 
     public void ajouterValeur(Object valeur) {
-        //noeudServeur.push(noeudsAjoutes);
+
+        noeudServeur.push().setValue(valeur);
+        noeudsAjoutes.add(noeudServeur);
 
     }
 
@@ -53,6 +63,12 @@ public class ProxyListe extends Proxy implements Fournisseur {
         childEventListener = new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                //Log.d("CleValues", "key1: "+key + "   KEY @ 2 : " + dataSnapshot.getKey());
+                //if (!dataSnapshot.getKey().equals(key) || key == null) {
+                //    Object valeurAjoutee = dataSnapshot.getValue();
+                //    actionNouvelItem.setArguments(Integer.parseInt(valeurAjoutee.toString()));
+                //    actionNouvelItem.executerDesQuePossible();
+                //}
 
             }
 
@@ -86,13 +102,12 @@ public class ProxyListe extends Proxy implements Fournisseur {
     @Override
     public void deconnecterDuServeur() {
         requete.removeEventListener(childEventListener);
-        //noeudServeur.child(noeudsAjoutes.).removeValue();
         super.deconnecterDuServeur();
     }
 
     @Override
     public void detruireValeurs() {
-
+        noeudServeur.removeValue();
     }
 
 
